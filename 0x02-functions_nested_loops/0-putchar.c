fa1d5671aa45ef9cfa554e1cc18cf6669606b264
bbenidar <bahmmokmnakt@gmail.com>
2023-02-22 13:14:19 -0800

    gg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
-int main(void)
-{
-	0-putchar('_');
-	0-putchar('p');
-	0-putchar('u');
-	0-putchar('t');
-	0-putchar('c');
-	0-putchar('h');
-	0-putchar('a');
-	0-putchar('r');
-	0-putchar('\n');
+#include "main.h"
 
-	return (0);
-}
+/**
+* main - print putchar
+*Return: always 0
+*/
+
+int main(void)
+  {    
+        _putchar('_');
+
+         _putchar('p');
+
+         _putchar('u');
+         _putchar('t');
+         _putchar('c');
+         _putchar('h');
+         _putchar('a');
+         _putchar('r');
+         _putchar('\n');
+       
+        return (0);
+  }
