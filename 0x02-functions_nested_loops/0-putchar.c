1a64d2590ca6aeff0490cea6bc50f6572c396f61
bbenidar <bahmmokmnakt@gmail.com>
2023-02-22 13:10:12 -0800

    jjj

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+int main(void)
+{
+	0-putchar('_');
+	0-putchar('p');
+	0-putchar('u');
+	0-putchar('t');
+	0-putchar('c');
+	0-putchar('h');
+	0-putchar('a');
+	0-putchar('r');
+	0-putchar('\n');
+
+	return (0);
+}
